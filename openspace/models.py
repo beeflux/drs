6f7b98a17f372b5216e8504946869c1a5349cc9e
awemulya <awemulya@gmail.com>
2019-07-20 13:07:51 +0545

    model

--- openspace/models.py
+++ openspace/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Space(models.Model):
+    name = models.CharField(max_length=100, default="Open Space")
+    location = models.PointField()
+    address = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
